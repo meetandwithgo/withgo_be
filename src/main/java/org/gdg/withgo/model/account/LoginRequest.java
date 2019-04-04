7d77fb0ebcf3b1c538c05dc0344f669c30817c5c
boxfox619 <boxfoxsg619@gmail.com>
2019-04-04 23:57:51 +0900

    Implement the login router

---
+++ src/main/java/org/gdg/withgo/model/account/LoginRequest.java
@@ -0,0 +1,30 @@
+package org.gdg.withgo.model.account;
+
+import org.gdg.withgo.model.Model;
+
+public class LoginRequest extends Model {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void assertFields() throws IllegalArgumentException {
+        valid(email, "Email not found");
+        valid(password, "Password not found");
+    }
+}
---
+++ src/test/java/org/gdg/withgo/LoginHandlerTest.java
@@ -0,0 +1,67 @@
+package org.gdg.withgo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.gdg.withgo.model.account.LoginRequest;
+import org.gdg.withgo.model.account.RegisterRequest;
+import org.gdg.withgo.service.Postgresql;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class LoginHandlerTest {
+
+    private Context getContextMock(){
+        Context contextMock = mock(Context.class);
+        LambdaLogger loggerMock = mock(LambdaLogger.class);
+        when(contextMock.getLogger()).thenReturn(loggerMock);
+        doAnswer(invocation -> {
+            System.out.println(invocation.getArguments()[0]);
+            return null;
+        }).when(loggerMock).log(anyString());
+        return contextMock;
+    }
+
+    @Before
+    public void registerHandlerTest(){
+        clear();
+        Context contextMock = getContextMock();
+        RegisterRequest request = new RegisterRequest();
+        RegisterHandler handler = new RegisterHandler();
+        request.setEmail("boxfoxsg619@gmail.com");
+        request.setName("boxfox");
+        request.setPassword("123456");
+        request.setPhone("010-3312-3123");
+        boolean res = handler.handleRequest(request, contextMock);
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void loginRequestTest(){
+        Context contextMock = getContextMock();
+        LoginRequest request = new LoginRequest();
+        LoginHandler handler = new LoginHandler();
+        request.setEmail("boxfoxsg619@gmail.com");
+        request.setPassword("123456");
+        boolean res = handler.handleRequest(request, contextMock);
+        Assert.assertTrue(res);
+    }
+
+    @After
+    public void clear(){
+        try(Connection connection = Postgresql.create()){
+            PreparedStatement stmt = connection.prepareStatement("delete from account where email='boxfoxsg619@gmail.com'");
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
