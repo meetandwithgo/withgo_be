d67ea2bd84bcd69ce9cee94d5d09d107626d1762
boxfox619 <boxfoxsg619@gmail.com>
2019-04-14 16:42:31 +0900

    Implement the ticket lookup repo & model

--- src/main/java/org/gdg/withgo/data/repository/postgres/TicketRepository.kt
+++ src/main/java/org/gdg/withgo/data/repository/postgres/TicketRepository.kt
@@ -5,16 +5,25 @@ import io.reactivex.Single
 import org.gdg.withgo.data.database.Tables.APPLICANT
 import org.gdg.withgo.data.database.Tables.TICKET
 import org.gdg.withgo.data.model.event.Ticket
+import org.gdg.withgo.data.repository.postgres.entity.TicketEntityMapper
 import org.gdg.withgo.domain.TicketUsecase
 import org.gdg.withgo.service.Postgresql
 
 class TicketRepository : TicketUsecase {
+    override fun addTickets(eventId: Int, tickets: List<Ticket>): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun updateTickets(eventId: Int, tickets: List<Ticket>): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun applyEvent(eventId: Int, ticketId: Int): Completable {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getUserTickets(uid: Int): Single<List<Ticket>> = Single.create {
-//        try {
+        //        try {
 //            Postgresql.dsl().use { dsl ->
 //                dsl.selectFrom(APPLICANT).where(APPLICANT.USER_ID.eq(uid).join).fetch()
 //                        .map {  }
@@ -24,8 +33,15 @@ class TicketRepository : TicketUsecase {
 //        }
     }
 
-    override fun getEventTickets(eventId: Int): Single<List<Ticket>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getEventTickets(eventId: Int): Single<List<Ticket>> = Single.create {
+        try {
+            val tickets = Postgresql.dsl().use { dsl ->
+                dsl.selectFrom(TICKET).where(TICKET.EVENT_ID.eq(eventId)).fetch().map { r -> TicketEntityMapper.fromRecord(r) }
+            }
+            it.onSuccess(tickets)
+        } catch (e: Throwable) {
+            it.onError(e)
+        }
     }
 
 }
